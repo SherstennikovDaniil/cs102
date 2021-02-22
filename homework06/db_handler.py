2fe5e9bf5d958707921fdbca9aac99a3238abad2
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2021-02-22 19:02:07 +0300

    Что-то не особо работает, но гит флоу сам себя не сделает

--- homework06/db_handler.py
+++ homework06/db_handler.py
@@ -3,9 +3,11 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from news_parser import get_news
+
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
+
 
 class News(Base):
     __tablename__ = "news"
@@ -17,20 +19,27 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
 
-s = session()
-def upload_to_db(news_list:list):
+
+
+
+def upload_to_db(news_list: list):
+    s = session()
     for post in news_list:
-        if s.query.filter_by(title=post.get('title')) is None:
-            news = News(title=post.get('title'),
-                        author=post.get('author'),
-                        url=post.get('url'),
-                        comments=post.get('comments'),
-                        points=post.get('points'))
+        if s.query(News.title == post.get('title')) is None:
+            news = News(
+                title=post.get("title"),
+                author=post.get("author"),
+                url=post.get("url"),
+                comments=post.get("comments"),
+                points=post.get("points"),
+            )
             s.add(news)
     s.commit()
 
-if __name__ == '__main__':
-    news_list = get_news(1)
-    upload_to_db(news_list)
+
+if __name__ == "__main__":
+    news = get_news(2)
+    upload_to_db(news)
