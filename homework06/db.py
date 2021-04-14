e8f929a4b4f9aebd6ecc01e5e57d31abe274063b
devdev20two <tobeteb524@edultry.com>
2021-04-14 07:07:55 +0300

    Прописал цикл заполнения бд, будем заполнять :3

--- homework06/db.py
+++ homework06/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from scraputils import get_news
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
@@ -21,3 +21,17 @@ class News(Base):
 
 
 Base.metadata.create_all(bind=engine)
+
+s = session()
+news = get_news('https://news.ycombinator.com/newest', 50)
+
+for article in news:
+    post = News(
+        title=article.get('title'),
+        author=article.get('author'),
+        url=article.get('url'),
+        comments=article.get('comments'),
+        points=article.get('points')
+    )
+    s.add(post)
+s.commit()
